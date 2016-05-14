7618759100e8a64b98fdd12cb7120f5035959db7
Eric Wickstrom <wickstrome@gmail.com>
2016-05-13 20:03:35 -0500

    Worked on some logic, still quite a few bugs to iron out.

--- app/src/main/java/com/ericwickstrom/uglycalculator/Calculator.java
+++ app/src/main/java/com/ericwickstrom/uglycalculator/Calculator.java
@@ -4,26 +4,43 @@ package com.ericwickstrom.uglycalculator;
  * Created by beardsmcgee on 5/13/16.
  */
 public class Calculator {
-    private float total;
+    //current operation in queue
+    public static int ADDITION = 1;
+    public static int SUBTRACTION = 2;
+    public static int MULTIPLICATION = 3;
+    public static int DIVISION = 4;
+    public static int NO_OPERATION = 0;
+    public int currentOperation = NO_OPERATION;
+    private float total = 0;
 
     Calculator() {
+
+    }
+
+
+    public void calculate(float number, int operation) {
+        if (currentOperation == NO_OPERATION) {
+            total = number;
+        } else {
+            if (currentOperation == ADDITION) {
+                total = total + number;
+            } else if (currentOperation == SUBTRACTION) {
+                total = total - number;
+            } else if (currentOperation == MULTIPLICATION) {
+                total = total * number;
+            } else if (currentOperation == DIVISION) {
+                total = total / number;
+            }
+        }
+        currentOperation = operation;
+    }
+
+    public void clear() {
         total = 0;
     }
 
-    public void add(float number){
-        total = total + number;
-    }
-
-    public void subtract(float number){
-        total = total - number;
-    }
-
-    public void multiply(float number){
-        total = total * number;
-    }
-
-    public void divide(float number){
-        total = total / number;
+    public int getCurrentOperation() {
+        return currentOperation;
     }
 
     public String getTotal(){
--- app/src/main/java/com/ericwickstrom/uglycalculator/MainActivity.java
+++ app/src/main/java/com/ericwickstrom/uglycalculator/MainActivity.java
@@ -6,8 +6,13 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //calculator object handles all of the logic (+, -, /, x, =) operations
     private Calculator calculator = new Calculator();
+
+    //this string is what is displayed to the user
     private String total = "";
+
+    private boolean firstNumericButtonPress = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,82 +22,113 @@ public class MainActivity extends AppCompatActivity {
 
     //event listener for button 1
     public void buttonPress1(View view){
+        firstNumericButtonPress();
         total = total + "1";
         updateDisplay();
     }
 
     //event listener for button 2
     public void buttonPress2(View view){
+        firstNumericButtonPress();
         total = total + "2";
         updateDisplay();
     }
 
     //event listener for button 3
     public void buttonPress3(View view){
+        firstNumericButtonPress();
         total = total + "3";
         updateDisplay();
     }
 
     //event listener for button 4
     public void buttonPress4(View view){
+        firstNumericButtonPress();
         total = total + "4";
         updateDisplay();
     }
 
     //event listener for button 5
     public void buttonPress5(View view){
+        firstNumericButtonPress();
         total = total + "5";
         updateDisplay();
     }
 
     //event listener for button 6
     public void buttonPress6(View view){
+        firstNumericButtonPress();
         total = total + "6";
         updateDisplay();
     }
 
     //event listener for button 7
     public void buttonPress7(View view){
+        firstNumericButtonPress();
         total = total + "7";
         updateDisplay();
     }
 
     //event listener for button 8
     public void buttonPress8(View view){
+        firstNumericButtonPress();
         total = total + "8";
         updateDisplay();
     }
 
     //event listener for button 9
     public void buttonPress9(View view){
+        firstNumericButtonPress();
         total = total + "9";
         updateDisplay();
     }
 
     //event listener for button 0
     public void buttonPress0(View view){
+        firstNumericButtonPress();
         total = total + "0";
         updateDisplay();
     }
 
     //event listener for button decimal
     public void buttonPressDecimal(View view){
-
+        if (!total.contains(".")) {
+            total = total + ".";
+        }
     }
 
-    //event listener for button multiply
-    public void buttonPressMultiply(View view){
-
+    //event listener for button addition
+    public void buttonPressAdd(View view) {
+        calculator.calculate(Float.valueOf(total), Calculator.ADDITION);
+        firstNumericButtonPress = true;
+        updateDisplay();
     }
 
     //event listener for button subtract
     public void buttonPressSubtract(View view){
+        calculator.calculate(Float.valueOf(total), Calculator.SUBTRACTION);
+        firstNumericButtonPress = true;
+        updateDisplay();
+    }
 
+    //event listener for button multiply
+    public void buttonPressMultiply(View view) {
+        calculator.calculate(Float.valueOf(total), Calculator.MULTIPLICATION);
+        firstNumericButtonPress = true;
+        updateDisplay();
+    }
+
+    public void buttonPressDivision(View view) {
+        calculator.calculate(Float.valueOf(total), Calculator.DIVISION);
+        firstNumericButtonPress = true;
+        updateDisplay();
     }
 
     //event listener for button clear
     public void buttonPressClear(View view){
-
+        total = "";
+        calculator.clear();
+        updateDisplay();
     }
 
     //event listener for button equal
@@ -100,13 +136,20 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void buttonPressAdd(View view){
-
+    private void firstNumericButtonPress() {
+        if (firstNumericButtonPress) {
+            total = "";
+            firstNumericButtonPress = false;
+        }
     }
 
     //updates display when pressing number buttons to construct a number
     private void updateDisplay(){
         TextView display = (TextView) findViewById(R.id.display);
-        display.setText(total);
+        if (firstNumericButtonPress) {
+            display.setText(calculator.getTotal());
+        } else {
+            display.setText(total);
+        }
     }
 }
